a00d07b5e4bedd8104c3c626b63864d404ec03b6
ko-lay926 <88261737+ko-lay926@users.noreply.github.com>
2026-06-14 11:03:12 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = "weeklyenglish-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./quizzes.json",
+  "./manifest.json",
+  "./logo.png"
+];
+
+/* Install */
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+
+  );
+
+  self.skipWaiting();
+});
+
+/* Activate */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+});
+
+/* Fetch */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
